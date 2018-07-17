f941219059bf7a96d0778ad57c993508615213e4
DeltaManiac <maxpaynered@gmail.com>
2018-07-17 21:30:57 +0530

    Moved to conrod + rodio

--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,80 @@
-extern crate button_controller;
-extern crate music;
-extern crate piston_window;
-use button_controller::{ButtonController, ButtonEvent, ButtonState};
-use piston_window::*;
+extern crate rodio;
 
-#[derive(Copy, Clone, Hash, PartialEq, Eq)]
-enum Music {
-    s1,
-}
-#[derive(Copy, Clone, Hash, PartialEq, Eq)]
-enum Sound {
-    s1,
-}
+use std::io::BufReader;
+
+//#[macro_use]
+extern crate conrod;
 
 fn main() {
-    let mut window: PistonWindow = WindowSettings::new("Test music", [640, 480])
-        .vsync(true)
-        .controllers(false)
-        .decorated(false)
-        .exit_on_esc(true)
-        .build()
-        .unwrap();
+        init();
+}
 
-    let mut quit = ButtonController::new();
-    let quit_pos = [0.0, 0.0, 20.0, 20.0];
 
-    let mut play_button = ButtonController::new();
-    let play_button_pos = [120.0, 120.0, 50.0, 50.0];
-    let mut is_playing = false;
-
-    music::start::<Music, Sound, _>(16, || {
-        music::bind_music_file(Music::s1, "./assets/test.mp3");
-        music::play_music(&Music::s1, music::Repeat::Forever);
-
-        music::set_volume(music::MIN_VOLUME);
-
-        'outer: while let Some(e) = window.next() {
-            quit.event(quit_pos, math::identity(), &e);
-            for e in &quit.events {
-                match e {
-                    ButtonEvent::Click => {
-                        println!("Clicked");
-                        break 'outer;
-                    }
-                    _ => (),
-                }
+    use conrod::{
+        backend::glium::glium::{self, Surface},
+    };
+    
+     fn init() {
+        let window = glium::glutin::WindowBuilder::new()
+            .with_title("rs-player")
+            .with_dimensions(320, 640);
+            let mut context= glium::glutin::ContextBuilder::new()
+            .with_vsync(true)
+            .with_multisampling(4);
+        let mut event_loop = glium::glutin::EventsLoop::new();
+            let display = glium::Display::new(window,context,&event_loop).unwrap();
+            let mut event_queue = Vec::new();
+            let mut renderer = conrod::backend::glium::Renderer::new(&display).unwrap();
+        let mut ui = conrod::UiBuilder::new([320 as f64,640 as f64]).build();
+        // NOTE(DeltaManiac): Here starts the main loop
+'main: loop {
+                event_queue.clear();
+                event_loop.poll_events(|event|{ event_queue.push(event);});
+        
+                if event_queue.is_empty() {
+                    event_loop.run_forever(|event| {
+                        event_queue.push(event);
+                    glium::glutin::ControlFlow::Break
+                });
             }
-
-            play_button.event(play_button_pos, math::identity(), &e);
-            for e in &play_button.events {
-                match e {
-                    ButtonEvent::Click => {
-                        println!("Current Playing status : {}", is_playing);
-
-                        if is_playing {
-                            music::play_music(&Music::s1, music::Repeat::Forever);
-
-                            music::set_volume(music::MAX_VOLUME);
-                        } else {
-                            music::set_volume(music::MIN_VOLUME);
+            for event in event_queue.drain(..) {
+                    match event.clone() {
+                        glium::glutin::Event::WindowEvent { event, .. } => {
+                            match event {
+                                glium::glutin::WindowEvent::Closed |
+                                glium::glutin::WindowEvent::KeyboardInput {
+                                    input: glium::glutin::KeyboardInput {
+                                        virtual_keycode: Some(glium::glutin::VirtualKeyCode::Escape),
+                                        ..
+                                },
+                                ..
+                            } => break 'main,
+                            _ => (),
                         }
-                        is_playing = !is_playing;
                     }
-
                     _ => (),
                 };
+            
+            let input = match conrod::backend::winit::convert_event(event, &display) {
+            None => continue,
+                    Some(input) => input,
+            };
+            
+            if let Some(primitives) = ui.draw_if_changed() {
+                    let mut target = display.draw();
+                    target.clear_color(0.0, 0.0, 0.0, 1.0);
+                    target.finish().unwrap();
             }
-
-            window.draw_2d(&e, |_c, g| {
-                clear([0.0; 4], g);
-
-                let quit_color = match quit.state(0.2) {
-                    ButtonState::Hover => [1.0, 0.0, 0.0, 1.0],
-                    ButtonState::Press => [0.1, 0.1, 0.1, 1.0],
-                    //ButtonState::Inactive => [0.4, 0.4, 0.4, 1.0],
-                    _ => [0.4, 0.4, 0.4, 1.0],
-                    //ButtonState::Cancel => [0.3, 0.2, 0.2, 1.0],
-                };
-                rectangle(quit_color, quit_pos, _c.transform, g);
-
-                let play_button_col = if is_playing {
-                    [0.0, 1.0, 0.0, 1.0]
-                } else {
-                    [0.0, 1.0, 1.0, 1.0]
-                };
-
-                rectangle(play_button_col, play_button_pos, _c.transform, g);
-            });
         }
-    });
+        
+    }
+}
+
+fn play_music() {
+    // TODO(DeltaManiac): Move Audio Code to a better location.
+    let device = rodio::default_output_device().unwrap();
+    let sink = rodio::Sink::new(&device);
+    let file = std::fs::File::open("./assets/test.mp3").unwrap();
+    sink.append(rodio::Decoder::new(BufReader::new(file)).unwrap());
+    sink.sleep_until_end();
 }
