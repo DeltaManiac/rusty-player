dd320a055ddf83ed369216dce9e300073eb0d0b7
DeltaManiac <maxpaynered@gmail.com>
2018-07-17 21:31:25 +0530

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -6,67 +6,64 @@ use std::io::BufReader;
 extern crate conrod;
 
 fn main() {
-        init();
+    init();
 }
 
+use conrod::backend::glium::glium::{self, Surface};
 
-    use conrod::{
-        backend::glium::glium::{self, Surface},
-    };
-    
-     fn init() {
-        let window = glium::glutin::WindowBuilder::new()
-            .with_title("rs-player")
-            .with_dimensions(320, 640);
-            let mut context= glium::glutin::ContextBuilder::new()
-            .with_vsync(true)
-            .with_multisampling(4);
-        let mut event_loop = glium::glutin::EventsLoop::new();
-            let display = glium::Display::new(window,context,&event_loop).unwrap();
-            let mut event_queue = Vec::new();
-            let mut renderer = conrod::backend::glium::Renderer::new(&display).unwrap();
-        let mut ui = conrod::UiBuilder::new([320 as f64,640 as f64]).build();
-        // NOTE(DeltaManiac): Here starts the main loop
-'main: loop {
-                event_queue.clear();
-                event_loop.poll_events(|event|{ event_queue.push(event);});
-        
-                if event_queue.is_empty() {
-                    event_loop.run_forever(|event| {
-                        event_queue.push(event);
-                    glium::glutin::ControlFlow::Break
-                });
-            }
-            for event in event_queue.drain(..) {
-                    match event.clone() {
-                        glium::glutin::Event::WindowEvent { event, .. } => {
-                            match event {
-                                glium::glutin::WindowEvent::Closed |
-                                glium::glutin::WindowEvent::KeyboardInput {
-                                    input: glium::glutin::KeyboardInput {
-                                        virtual_keycode: Some(glium::glutin::VirtualKeyCode::Escape),
-                                        ..
-                                },
+fn init() {
+    let window = glium::glutin::WindowBuilder::new()
+        .with_title("rs-player")
+        .with_dimensions(320, 640);
+    let mut context = glium::glutin::ContextBuilder::new()
+        .with_vsync(true)
+        .with_multisampling(4);
+    let mut event_loop = glium::glutin::EventsLoop::new();
+    let display = glium::Display::new(window, context, &event_loop).unwrap();
+    let mut event_queue = Vec::new();
+    let mut renderer = conrod::backend::glium::Renderer::new(&display).unwrap();
+    let mut ui = conrod::UiBuilder::new([320 as f64, 640 as f64]).build();
+    // NOTE(DeltaManiac): Here starts the main loop
+    'main: loop {
+        event_queue.clear();
+        event_loop.poll_events(|event| {
+            event_queue.push(event);
+        });
+
+        if event_queue.is_empty() {
+            event_loop.run_forever(|event| {
+                event_queue.push(event);
+                glium::glutin::ControlFlow::Break
+            });
+        }
+        for event in event_queue.drain(..) {
+            match event.clone() {
+                glium::glutin::Event::WindowEvent { event, .. } => match event {
+                    glium::glutin::WindowEvent::Closed
+                    | glium::glutin::WindowEvent::KeyboardInput {
+                        input:
+                            glium::glutin::KeyboardInput {
+                                virtual_keycode: Some(glium::glutin::VirtualKeyCode::Escape),
                                 ..
-                            } => break 'main,
-                            _ => (),
-                        }
-                    }
+                            },
+                        ..
+                    } => break 'main,
                     _ => (),
-                };
-            
-            let input = match conrod::backend::winit::convert_event(event, &display) {
-            None => continue,
-                    Some(input) => input,
+                },
+                _ => (),
             };
-            
+
+            let input = match conrod::backend::winit::convert_event(event, &display) {
+                None => continue,
+                Some(input) => input,
+            };
+
             if let Some(primitives) = ui.draw_if_changed() {
-                    let mut target = display.draw();
-                    target.clear_color(0.0, 0.0, 0.0, 1.0);
-                    target.finish().unwrap();
+                let mut target = display.draw();
+                target.clear_color(0.0, 0.0, 0.0, 1.0);
+                target.finish().unwrap();
             }
         }
-        
     }
 }
 
